e0b3b0e4492f7164af4d41aa22efc6975f02e8e4
Jesús Almansa <jtafernandez@gmail.com>
2024-09-30 09:36:30 +0000

    refactor

--- create_project_structure.sh
+++ create_project_structure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the directories and files you want to create
-folders=("Notebooks" "data" "doc" "src" "src.egg-info")
-files=(".gitignore" "README.md" "setup.py" "src/__init__.py")
+folders=("Notebooks" "data" "doc" "src")
+files=(".gitignore" "setup.py" "src/__init__.py")
 
 # Create the directories
 for folder in "${folders[@]}"; do
@@ -16,9 +16,9 @@ for file in "${files[@]}"; do
     echo "Created file: $file"
     
     # Add some initial content to each file
-    if [ "$file" == "README.md" ]; then
-        echo -e "# Project Title\n\nProject description goes here." > "$file"
-    elif [ "$file" == ".gitignore" ]; then
+    # if [ "$file" == "README.md" ]; then
+    #     echo -e "# Project Title\n\nProject description goes here." > "$file"
+    if [ "$file" == ".gitignore" ]; then
         echo -e "# Add files or directories to ignore for version control" > "$file"
     elif [ "$file" == "setup.py" ]; then
         echo -e "from setuptools import setup, find_packages\n\nsetup(\n    name='your_project_name',\n    version='0.1.0',\n    packages=find_packages(where='src'),\n    package_dir={'': 'src'},\n    install_requires=[\n        # Add your project dependencies here\n    ],\n    author='Your Name',\n    description='A description of your project" > "$file"
